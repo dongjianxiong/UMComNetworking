c8b96b48d6eb046e23f3537d83652cde8beef10e
jianxiong.djx <jianxiong.djx@alibaba-inc.com>
2015-06-11 10:28:04 +0800

    Initial Commit

---
+++ UMComNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UMComNetworking
+//
+//  Created by umeng on 15/6/11.
+//  Copyright (c) 2015年 dongjianxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UMComNetworking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UMComNetworking
+//
+//  Created by umeng on 15/6/11.
+//  Copyright (c) 2015年 dongjianxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
