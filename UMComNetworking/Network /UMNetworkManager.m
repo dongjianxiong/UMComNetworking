fc441cefcb3ad8eddf37ae02fb3f51c28f2841c7
jianxiong.djx <jianxiong.djx@alibaba-inc.com>
2015-08-15 09:22:23 +0800

    增加网络请求操作文件和网络管理文件

---
+++ UMComNetworking/Network /UMNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  UMNetworkManager.m
+//  UMComNetworking
+//
+//  Created by umeng on 15/8/15.
+//  Copyright (c) 2015年 dongjianxiong. All rights reserved.
+//
+
+#import "UMNetworkManager.h"
+
+@implementation UMNetworkManager
+
+@end
---
+++ UMComNetworking/Network /UMURLConnectionOperation.m
@@ -0,0 +1,13 @@
+//
+//  UMURLConnectionOperation.m
+//  UMComNetworking
+//
+//  Created by umeng on 15/8/15.
+//  Copyright (c) 2015年 dongjianxiong. All rights reserved.
+//
+
+#import "UMURLConnectionOperation.h"
+
+@implementation UMURLConnectionOperation
+
+@end
